9b67940d845364dc34062d372b4d79f00aa7854c
xueyinhu <13453894389@163.com>
2023-07-22 21:06:25 +0800

    start

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from '@/App.vue'
+import PlatformTop from '@/components/platform_top/index.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('PlatformTop', PlatformTop)
+
+app.mount('#app')
