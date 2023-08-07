98919b27751ade2fa66dd758c70365d3278fcae6
xueyinhu <13453894389@163.com>
2023-08-07 23:27:58 +0800

    start

---
+++ src/components/super_resolution/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="platform_top">
+        <div class="content">
+            Top
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+    .platform_top {
+        width: 100%;
+        height: 70px;
+        background: #fff;
+        .content {
+            
+        }
+    }
+</style>
